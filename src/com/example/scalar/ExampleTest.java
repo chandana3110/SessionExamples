176db8f4cf7e64c915f8909166c745b66e554d95
chandana3110 <102597155+chandana3110@users.noreply.github.com>
2022-04-07 17:30:16 +0530

    Initial commit

---
+++ src/com/example/scalar/ExampleTest.java
@@ -0,0 +1,9 @@
+package com.example.scalar;
+
+import java.util.Arrays;
+
+public class ExampleTest {
+    public static void main(String[] args) {
+        System.out.println("args = " + Arrays.deepToString(args));
+    }
+}
